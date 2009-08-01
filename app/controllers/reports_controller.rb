5a2da4003e57ff1073fe62d2963db438672f8c9c
James Darling <james@abscond.org>
2009-08-01 13:42:21 +0100

    small tweaks

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,23 +1,20 @@
 class ReportsController < ApplicationController
   def index
     if params[:report]
-      create
-      render :action => "create"
+      values = params[:report].values.compact.delete_if{|x| x==""}
+      if values.empty?
+        render :text => "You do, however, have to fill in *something*" and return
+      else
+        @report = Report.calculate values
+        render :action => "create"
+      end
     else
-      new
+      @characteristic_categories = Characteristic.find(:all, :group => "category", :select => "category")
+      @characteristics = {}
+      @characteristic_categories.each do |category|
+        @characteristics[category.category] = Characteristic.find(:all, :conditions => ["category = ?", category.category])
+      end
       render :action => "new"
     end
-  end
-  def new
-    #dirty
-    @characteristic_categories = Characteristic.find(:all, :group => "category", :select => "category")
-    @characteristics = {}
-    @characteristic_categories.each do |category|
-      @characteristics[category.category] = Characteristic.find(:all, :conditions => ["category = ?", category.category])
-    end
-  end
-  
-  def create
-    @report = Report.calculate params[:report].values.compact.delete_if{|x| x==""}
   end
 end
